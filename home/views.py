f422720238a1509485f7afe286271051d55a3512
bobantonbob <130400459+bobantonbob@users.noreply.github.com>
2024-04-23 00:18:14 +0300

    переніс все у корень

---
+++ home/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+
+
+def index(request):
+    return render(request, 'home/index.html', {
+        'title': 'Головна',
+        'page': 'index',
+        'app': 'home'
+    })
+
+def about(request):
+    return render(request, 'home/about.html', {
+        'title': 'Про сайт',
+        'page': 'about',
+        'app': 'home'
+    })
+
+def contacts(request):
+    return render(request, 'home/contacts.html', {
+        'title': 'Контакти',
+        'page': 'contacts',
+        'app': 'home'
+    })
+
+def news(request):
+    return render(request, 'home/news.html', {
+        'title': 'Новини',
+        'page': 'news',
+        'app': 'home'
+    })
+
+
--- root/news/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'news'
-
-urlpatterns = [
-
-    path('', views.index, name='news'),  # Шлях для сторінки новин
-]
