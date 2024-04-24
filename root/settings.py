bb2f1965e86b7ca384a8c63c5b25edb5d9d6024a
ANTON BABENKO <antonbabenko1983@gmail.com>
2024-04-24 18:07:05 +0300

    Update settings.py
    DEBUG = env('DEBUG') for koeb

--- root/settings.py
+++ root/settings.py
@@ -33,11 +33,11 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-# DEBUG = env('DEBUG')
+# DEBUG = True
+DEBUG = env('DEBUG')
 
-# ALLOWED_HOSTS = ['final-project-goit-web-antonbabenko.koyeb.app']
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['final-project-goit-web-antonbabenko.koyeb.app']
+# ALLOWED_HOSTS = []
 
 # Application definition
 
