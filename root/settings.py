c865de26abffcb06d986b9cba527470ddc8fd607
ANTON BABENKO <antonbabenko1983@gmail.com>
2024-04-24 21:03:53 +0300

    Feature/fox web ticket0 (#5)
    * назва base title
    
    * Templates of contacts and notes have been made.
    Searching and sorting of contacts and notes in JavaScript is done.
    Made a configuration and login profile template.
    Made and configured routes for performing operations from contacts and notes. (deletion and change).
    Made delete and change templates for contacts and notes.
    
    * images for *.mim.js  for deployment on koeb
    
    * changes .env
    filling - staticfiles\*
    
    * settings DEBUG

--- root/settings.py
+++ root/settings.py
@@ -96,12 +96,20 @@ DATABASES = {
         # 'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': BASE_DIR / 'db.sqlite3',
 
+        # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        # 'NAME': env('NAME'),
+        # 'USER': env('USER_DB'),
+        # 'PASSWORD': env('PASSWORD'),
+        # 'HOST': env('HOST'),
+        # 'PORT': env('PORT_DB'),
+
+
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': env('NAME'),
-        'USER': env('USER_DB'),
-        'PASSWORD': env('PASSWORD'),
-        'HOST': env('HOST'),
-        'PORT': env('PORT_DB'),
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': env('DB_PORT'),
 
 
 
