05602457702d47b123e88645d83ebb09cd01f803
bobantonbob <130400459+bobantonbob@users.noreply.github.com>
2024-04-25 18:19:45 +0300

    corrected js tables deleting redundant files js migration to the cloud for s3

--- root/settings.py
+++ root/settings.py
@@ -96,6 +96,10 @@ WSGI_APPLICATION = 'root.wsgi.application'
 
 DATABASES = {
     'default': {
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+
+
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': env('DB_NAME'),
         'USER': env('DB_USER'),
--- s3_storage/apps.py
+++ s3_storage/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 class S3StorageConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 's3_storage'
-    path = '/app/s3_storage'
+    # path = '/app/s3_storage'
