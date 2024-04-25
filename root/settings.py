991fb3b622d5d11aec8df9c3921388ffd92a9ab4
bobantonbob <130400459+bobantonbob@users.noreply.github.com>
2024-04-25 13:03:52 +0300

    'ENGINE' and ataticfiles

--- root/settings.py
+++ root/settings.py
@@ -96,6 +96,7 @@ WSGI_APPLICATION = 'root.wsgi.application'
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': env('DB_NAME'),
         'USER': env('DB_USER'),
         'PASSWORD': env('DB_PASSWORD'),
---
+++ staticfiles/vendor/overlay-scroll/custom-scrollbar.f64273ad782a.js
@@ -0,0 +1,115 @@
+$(function() {
+	$(".sidebarMenuScroll").overlayScrollbars({
+		scrollbars : {
+			visibility       : "auto",
+			autoHide         : "scroll",
+			autoHideDelay    : 200,
+			dragScrolling    : true,
+			clickScrolling   : false,
+			touchSupport     : true,
+			snapHandle       : false,
+		},
+	});
+});
+
+
+$(function() {
+	$(".content-wrapper-scroll").overlayScrollbars({
+		scrollbars : {
+			visibility       : "auto",
+			autoHide         : "scroll",
+			autoHideDelay    : 200,
+			dragScrolling    : true,
+			clickScrolling   : false,
+			touchSupport     : true,
+			snapHandle       : false,
+		},
+	});
+});
+
+
+// Scroll 240
+$(function() {
+	$(".scroll240").overlayScrollbars({
+		scrollbars : {
+			visibility       : "auto",
+			autoHide         : "scroll",
+			autoHideDelay    : 200,
+			dragScrolling    : true,
+			clickScrolling   : false,
+			touchSupport     : true,
+			snapHandle       : false,
+		},
+	});
+});
+
+
+// Scroll 370
+$(function() {
+	$(".scroll370").overlayScrollbars({
+		scrollbars : {
+			visibility       : "auto",
+			autoHide         : "scroll",
+			autoHideDelay    : 200,
+			dragScrolling    : true,
+			clickScrolling   : false,
+			touchSupport     : true,
+			snapHandle       : false,
+		},
+	});
+});
+
+
+
+
+// Scroll 300
+$(function() {
+	$(".scroll300").overlayScrollbars({
+		scrollbars : {
+			visibility       : "auto",
+			autoHide         : "scroll",
+			autoHideDelay    : 200,
+			dragScrolling    : true,
+			clickScrolling   : false,
+			touchSupport     : true,
+			snapHandle       : false,
+		},
+	});
+});
+
+
+
+
+
+// Scroll 206
+$(function() {
+	$(".scroll206").overlayScrollbars({
+		scrollbars : {
+			visibility       : "auto",
+			autoHide         : "scroll",
+			autoHideDelay    : 200,
+			dragScrolling    : true,
+			clickScrolling   : false,
+			touchSupport     : true,
+			snapHandle       : false,
+		},
+	});
+});
+
+
+// Scroll 250
+$(function() {
+	$(".scroll250").overlayScrollbars({
+		scrollbars : {
+			visibility       : "auto",
+			autoHide         : "scroll",
+			autoHideDelay    : 200,
+			dragScrolling    : true,
+			clickScrolling   : false,
+			touchSupport     : true,
+			snapHandle       : false,
+		},
+	});
+});
+
+
