b7680abd0e61045142c5ab41cf6d6a3c0a659d3f
bobantonbob <130400459+bobantonbob@users.noreply.github.com>
2024-04-26 17:39:37 +0300

    updated templates CRUD and routes for files, notes and сontacs

--- comander/urls.py
+++ comander/urls.py
@@ -10,18 +10,24 @@ urlpatterns = [
     path('search_all', views.search_all, name='search_all'),
     path('change', views.change, name='change'),
     path('delete', views.delete, name='delete'),
+    path('deleted_done', views.deleted_done, name='deleted_done'),
     path('sorting_files', views.sorting_files, name='sorting_files'),
     path('birhday_contact', views.birhday_contact, name='birhday_contact'),
     path('download', views.download, name='download'),
     path('view_file', views.view_file, name='view_file'),
-    path('contact_card', views.contact_card, name='contact_card'),
-    path('deleted_done', views.deleted_done, name='deleted_done'),
+    path('contact_card', views.contact_card, name='contact_card'),    
     path('change_done', views.change_done, name='change_done'),
     path('add_note', views.add_note, name='add_note'),
     path('all_notes', views.all_notes, name='all_notes'),
     path('change_note', views.change_note, name='change_note'),
     path('change_note_done', views.change_note_done, name='change_note_done'),
-    
+    path('deleted_note', views.deleted_note, name='deleted_note'),
+    path('files', views.files, name='files'),
+    path('file_upload_done', views.file_upload_done, name='file_upload_done'),
+    path('change_file', views.change_file, name='change_file'),
+    path('change_file_done', views.change_file_done, name='change_file_done'),
+    path('deleted_file_done', views.deleted_file_done, name='deleted_file_done'),
+
 
 
 
