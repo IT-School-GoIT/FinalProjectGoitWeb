cfd4564117abe6f83aca0c19f77f0d89f3ef4da1
Vitalii Bielimov <vbit2000@gmail.com>
2024-04-28 17:34:00 +0300

    Ticket_9 complete

--- s3_storage/urls.py
+++ s3_storage/urls.py
@@ -7,9 +7,10 @@ urlpatterns = [
     path('upload/', views.upload_file, name='upload'),
     path('list/', views.file_list, name='list'),
     path('download/<int:file_id>/', views.download_file, name='download'),
+    path('edit_file/<int:file_id>/', views.edit_file, name='edit_file'),
     path('delete/<int:file_id>/', views.delete_file, name='delete'),
     path('create_category/', views.create_category, name='create_category'),
     path('edit_category/<int:category_id>/', views.edit_category, name='edit_category'),
     path('delete_category/<int:category_id>/', views.delete_category, name='delete_category'),
-    # path('files', views.files, name='files'),
+    path('categories/', views.category_list, name='category_list'),
 ]
