d79ec0bb93dc270dfb98f9e879498f62d97349f8
Vitalii Bielimov <vbit2000@gmail.com>
2024-04-29 15:16:07 +0300

    Implemented password recovery for users by email

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,8 @@
+from django.contrib.auth.views import PasswordResetView
+from django.contrib.messages.views import SuccessMessageMixin
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.urls import reverse_lazy
 from django.views import View
 from django.contrib import messages
 
@@ -48,5 +51,13 @@ def profile_settings(request):
         'app': 'accounts'
     })
 
+
+class ResetPasswordView(SuccessMessageMixin, PasswordResetView):
+    template_name = 'accounts/password_reset.html'
+    email_template_name = 'accounts/password_reset_email.html'
+    html_email_template_name = 'accounts/password_reset_email.html'
+    success_url = reverse_lazy('accounts:password_reset_done')
+    success_message = "An email with instructions to reset your password has been sent to %(email)s."
+    subject_template_name = 'accounts/password_reset_subject.txt'
 
         
